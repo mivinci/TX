mivinci/TPKit

WIP Update version

Source/Transport/task/TaskManagerTest.cc
@@ -1,0 +1,18 @@
+#include "Transport/task/TaskManager.h"
+#include "gtest/gtest.h"
+
+namespace TP::task {
+class TaskManagerTest : public testing::Test {};
+
+TEST_F(TaskManagerTest, genTaskId) {
+  const int factor = 10, span = 1;
+  TaskManager mgr(factor, span);
+  for (int i = 0; i < factor; i++) {
+    int32_t id = mgr.createTask(Task::Kind::Auto);
+    EXPECT_EQ(id / factor, static_cast<int32_t>(Task::Kind::Auto));
+    EXPECT_EQ(id % factor, i);
+  }
+  EXPECT_EQ(mgr.createTask(), -1);
+}
+
+} // namespace TP::task

Source/TX/logging/Logger.h
@@ -1,0 +1,88 @@
+#pragma once
+#include "TX/time/Time.h"
+#include "fmt/printf.h"
+#include <string_view>
+#include <utility>
+
+namespace TX::logging {
+#define TX_LOG(level, scope, ...)                                              \
+  for (bool shouldLog =                                                        \
+           TX::logging::Logger::shouldLog(TX::logging::Logger::Level::level);  \
+       shouldLog; shouldLog = false)                                           \
+  TX::logging::Logger::Log(TX::logging::Logger::Level::level, __FILE__,        \
+                           __LINE__, __FUNCTION__, scope, __VA_ARGS__)         \
+      .output()
+
+#define TX_THROW(...)                                                          \
+  for (TX::logging::Logger::Log log(TX::logging::Logger::Level::Error,         \
+                                    __FILE__, __LINE__, __FUNCTION__, "throw", \
+                                    __VA_ARGS__);                              \
+       ; log.throwException())
+
+#define TX_DEBUG(...) TX_LOG(Debug, "TX", __VA_ARGS__)
+#define TX_INFO(...) TX_LOG(Info, "TX", __VA_ARGS__)
+#define TX_WARN(...) TX_LOG(Warn, "TX", __VA_ARGS__)
+#define TX_ERROR(...) TX_LOG(Error, "TX", __VA_ARGS__)
+#define TX_FATAL(...) TX_LOG(Fatal, "TX", __VA_ARGS__)
+
+#define TX_DEBUG_SCOPE(scope, ...) TX_LOG(Debug, scope, __VA_ARGS__)
+#define TX_INFO_SCOPE(scope, ...) TX_LOG(Info, scope, __VA_ARGS__)
+#define TX_WARN_SCOPE(scope, ...) TX_LOG(Warn, scope, __VA_ARGS__)
+#define TX_ERROR_SCOPE(scope, ...) TX_LOG(Error, scope, __VA_ARGS__)
+#define TX_FATAL_SCOPE(scope, ...) TX_LOG(Fatal, scope, __VA_ARGS__)
+
+class Logger {
+public:
+  enum class Level { Trace, Debug, Info, Warn, Error, Fatal };
+  class Log {
+  public:
+    template <typename... Args>
+    explicit Log(Level level, const char *file, int line, const char *function,
+                 const char *scope, const char *format, Args &&...args)
+        : level(level), line(line), file(file), scope(scope),
+          function(function) {
+      message = fmt::sprintf(format, args...);
+      clean();
+    }
+    explicit Log(Level level, const char *file, int line, const char *function,
+                 const char *scope)
+        : level(level), line(line), file(file), scope(scope),
+          function(function) {
+      clean();
+    }
+    Log(Log &) = delete;
+    void output() const;
+    void throwException() const;
+
+  private:
+    void clean();
+
+  public:
+    Level level;
+    int line;
+    std::string_view file;
+    std::string_view scope;
+    std::string_view function;
+    std::string message;
+    TX::time::Time time;
+  };
+
+  class Reporter {
+  public:
+    virtual void report(const Log &);
+  };
+
+  class Formatter {
+  public:
+    virtual std::string format(const Log &);
+  };
+
+  static Logger::Level level_;
+  static Logger::Reporter *reporter_;
+  static Logger::Formatter *formatter_;
+  static void setLevel(Logger::Level level) { level_ = level; }
+  static void setReporter(Reporter *r) { reporter_ = r; }
+  static void setFormatter(Formatter *f) { formatter_ = f; }
+  static bool shouldLog(Logger::Level level) { return level >= level_; }
+};
+} // namespace TX::logging

Source/TX/time/Clock.h
@@ -1,0 +1,66 @@
+#pragma once
+#include <cstdint>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <ctime>
+#endif
+
+namespace TX::time {
+class Clock {
+public:
+  enum struct Id {
+    Real = 0,
+    Monotonic = 6,
+  };
+  struct TimePoint {
+    int64_t sec;
+    int64_t nsec;
+  };
+  static Clock::TimePoint time(Clock::Id id) {
+    Clock::TimePoint tp{};
+#ifdef _WIN32
+    switch (id) {
+    case Clock::Id::Real:
+      FILETIME ft;
+      GetSystemTimePreciseAsFileTime(&ft);
+      // 将 FileTime 转换为 100 纳秒单位
+      ULARGE_INTEGER uli; //
+      uli.LowPart = ft.dwLowDateTime;
+      uli.HighPart = ft.dwHighDateTime;
+      // 转换为 Unix 时间戳（从 1601-01-01 调整为 1970-01-01）
+      uli.QuadPart -= 116444736000000000ULL;
+      // 转换为秒和纳秒
+      tp.sec = uli.QuadPart / 10'000'000;
+      tp.nsec = (uli.QuadPart % 10'000'000) * 100;
+      break;
+    case Clock::Id::Monotonic:
+      static LARGE_INTEGER freq;
+      if (!inited) {
+        QueryPerformanceFrequency(&freq);
+        inited = true;
+      }
+      LARGE_INTEGER count;
+      QueryPerformanceCounter(&count);
+      tp.sec = count.QuadPart / freq.QuadPart;
+      tp.nsec = (count.QuadPart % freq.QuadPart) * 1000000000LL / freq.QuadPart;
+      break;
+    }
+#else
+    struct timespec ts {};
+    clock_gettime(static_cast<clockid_t>(id), &ts);
+    tp.sec = ts.tv_sec;
+    tp.nsec = ts.tv_nsec;
+#endif
+    return tp;
+  };
+
+  static Clock::TimePoint real() { return time(Clock::Id::Real); }
+  static Clock::TimePoint monotonic() { return time(Clock::Id::Monotonic); }
+
+private:
+  static bool inited;
+};
+
+inline bool Clock::inited = false;
+} // namespace TX::time

Source/TX/time/Duration.h
@@ -1,0 +1,67 @@
+#pragma once
+#include "TX/time/Clock.h"
+
+namespace TX::time {
+class Duration final {
+public:
+  explicit Duration(int64_t n) : inner(n) {}
+  static Duration second(int n) { return Duration(n * 1'000'000'000); }
+  static Duration minute(int n) { return Duration(n * 1'000'000'000 * 60); }
+  static Duration hour(int n) { return Duration(n * 1'000'000'000 * 60 * 60); }
+
+  [[nodiscard]] int64_t nanoSeconds() const { return inner; }
+  [[nodiscard]] int64_t microSeconds() const { return inner / 1'000; }
+  [[nodiscard]] int64_t milliSeconds() const { return inner / 1'000'000; }
+
+  [[nodiscard]] double seconds() const {
+    int64_t sec = inner / 1'000'000'000;
+    int64_t nsec = inner % 1'000'000'000;
+    return static_cast<double>(sec) + static_cast<double>(nsec) / 1e9;
+  }
+  [[nodiscard]] double minutes() const {
+    int64_t min = inner / (60 * 1'000'000'000LL);
+    int64_t nsec = inner % (60 * 1'000'000'000LL);
+    return static_cast<double>(min) + static_cast<double>(nsec) / (60 * 1e9);
+  }
+  [[nodiscard]] double hours() const {
+    int64_t hour = inner / (60 * 60 * 1'000'000'000LL);
+    int64_t nsec = inner % (60 * 60 * 1'000'000'000LL);
+    return static_cast<double>(hour) +
+           static_cast<double>(nsec) / (60 * 60 * 1e9);
+  }
+
+  [[nodiscard]] Duration truncate(Duration m) const {
+    return Duration(m.inner <= 0 ? inner : (inner - inner % m.inner));
+  }
+
+  Duration operator+(Duration &other) const {
+    return Duration(inner + other.inner);
+  }
+  Duration operator-(Duration &other) const {
+    return Duration(inner - other.inner);
+  }
+  Duration operator*(Duration &other) const {
+    return Duration(inner * other.inner);
+  }
+  Duration operator/(Duration &other) const {
+    return Duration(inner / other.inner);
+  }
+  Duration operator%(Duration &other) const {
+    return Duration(inner % other.inner);
+  }
+
+  [[nodiscard]] Clock::TimePoint toTimePoint() const {
+    Clock::TimePoint tp{};
+    int64_t sec = inner / 1'000'000'000LL;
+    int64_t nsec = inner % 1'000'000'000LL;
+    tp.sec = sec;
+    tp.nsec = nsec;
+    return tp;
+  }
+
+private:
+  friend class Time;
+  int64_t inner;
+};
+
+}; // namespace TX::time

Source/TX/time/Time.h
@@ -1,0 +1,66 @@
+#pragma once
+#include "TX/time/Clock.h"
+#include "TX/time/Duration.h"
+
+namespace TX::time {
+constexpr uint64_t NANO_MASK = (1 << 30) - 1;
+
+class Time final {
+public:
+  explicit Time() {
+    auto real_tp = Clock::real();
+    auto mono_tp = Clock::monotonic();
+    real_ = (real_tp.sec << 31) | (real_tp.nsec);
+    mono_ = mono_tp.sec * 1'000'000'000 + mono_tp.nsec;
+  }
+
+  [[nodiscard]] uint64_t unix() const { return sec(); }
+  [[nodiscard]] uint64_t unixMilli() const {
+    return sec() * 1'000 + nsec() / 1'000'000;
+  }
+  [[nodiscard]] uint64_t unixMicro() const {
+    return sec() / 1'000'000 + nsec() / 1'000;
+  }
+  [[nodiscard]] uint64_t unixNano() const {
+    return sec() * 1'000'000'000 + nsec();
+  }
+
+  Time operator+(Duration d) {
+    int64_t d_sec = d.inner / 1'000'000'000;
+    int64_t d_nsec = d.inner % 1'000'000'000;
+    uint64_t t_sec = sec();
+    uint64_t t_nsec = nsec();
+    real_ = ((t_sec + d_sec) << 31) | (t_nsec + d_nsec);
+    mono_ += d.inner;
+    return *this;
+  }
+  Time operator-(Duration d) {
+    int64_t d_sec = d.inner / 1'000'000'000;
+    int64_t d_nsec = d.inner % 1'000'000'000;
+    int64_t t_sec = sec();
+    int64_t t_nsec = nsec();
+    real_ = ((t_sec - d_sec) << 31) | (t_nsec - d_nsec);
+    mono_ -= d.inner;
+    return *this;
+  }
+  Duration operator-(Time other) {
+    return Duration((sec() - other.sec()) * 1'000'000'000 +
+                    (nsec() + other.nsec()));
+  }
+
+private:
+  [[nodiscard]] int64_t sec() const {
+    return static_cast<int64_t>(real_ >> 31);
+  }
+  [[nodiscard]] int32_t nsec() const {
+    return static_cast<int32_t>(real_ & NANO_MASK);
+  }
+
+private:
+  uint64_t real_;
+  uint64_t mono_;
+};
+
+inline Duration since(Time t) { return Time() - t; }
+inline Duration until(Time t) { return t - Time(); }
+} // namespace TX::time

mivinci/TX

Source/Kernel/task/TaskManagerTest.cc
@@ -1,0 +1,18 @@
+#include "Kernel/task/TaskManager.h"
+#include "gtest/gtest.h"
+
+namespace TP::task {
+class TaskManagerTest : public testing::Test {};
+
+TEST_F(TaskManagerTest, genTaskId) {
+  const int factor = 10, span = 1;
+  TaskManager mgr(factor, span);
+  for (int i = 0; i < factor; i++) {
+    int32_t id = mgr.createTask(Task::Kind::Auto);
+    EXPECT_EQ(id / factor, static_cast<int32_t>(Task::Kind::Auto));
+    EXPECT_EQ(id % factor, i);
+  }
+  EXPECT_EQ(mgr.createTask(), -1);
+}
+
+}  // namespace TP::task

Source/TX/logging/Logger.h
@@ -1,0 +1,96 @@
+#pragma once
+#include <string_view>
+
+#include "TX/time/Time.h"
+#include "fmt/base.h"
+#include "fmt/compile.h"
+#include "fmt/format.h"
+
+namespace TX::logging {
+#define TX_LOG(level, scope, ...)                                             \
+  for (bool shouldLog =                                                       \
+           TX::logging::Logger::shouldLog(TX::logging::Logger::Level::level); \
+       shouldLog; shouldLog = false)                                          \
+  TX::logging::Logger::Log(TX::logging::Logger::Level::level, __FILE__,       \
+                           __LINE__, __FUNCTION__, scope, __VA_ARGS__)        \
+      .output()
+
+#define TX_THROW(...)                                                          \
+  for (TX::logging::Logger::Log log(TX::logging::Logger::Level::Error,         \
+                                    __FILE__, __LINE__, __FUNCTION__, "throw", \
+                                    __VA_ARGS__);                              \
+       ; log.throwException())
+
+#define TX_DEBUG(...) TX_LOG(Debug, "TX", __VA_ARGS__)
+#define TX_INFO(...) TX_LOG(Info, "TX", __VA_ARGS__)
+#define TX_WARN(...) TX_LOG(Warn, "TX", __VA_ARGS__)
+#define TX_ERROR(...) TX_LOG(Error, "TX", __VA_ARGS__)
+#define TX_FATAL(...) TX_LOG(Fatal, "TX", __VA_ARGS__)
+
+#define TX_DEBUG_SCOPE(scope, ...) TX_LOG(Debug, scope, __VA_ARGS__)
+#define TX_INFO_SCOPE(scope, ...) TX_LOG(Info, scope, __VA_ARGS__)
+#define TX_WARN_SCOPE(scope, ...) TX_LOG(Warn, scope, __VA_ARGS__)
+#define TX_ERROR_SCOPE(scope, ...) TX_LOG(Error, scope, __VA_ARGS__)
+#define TX_FATAL_SCOPE(scope, ...) TX_LOG(Fatal, scope, __VA_ARGS__)
+
+class Logger {
+ public:
+  enum class Level { Trace, Debug, Info, Warn, Error, Fatal };
+  class Log {
+   public:
+    template <typename... Args>
+    explicit Log(Level level, const char *file, int line, const char *function,
+                 const char *scope, const char *format, Args &&...args)
+        : level(level),
+          line(line),
+          file(file),
+          scope(scope),
+          function(function),
+          message(fmt::format(fmt::runtime(format), args...)) {
+      clean();
+    }
+    explicit Log(Level level, const char *file, int line, const char *function,
+                 const char *scope)
+        : level(level),
+          line(line),
+          file(file),
+          scope(scope),
+          function(function) {
+      clean();
+    }
+    Log(Log &) = delete;
+    void output() const;
+    void throwException() const;
+
+   private:
+    void clean();
+
+   public:
+    Level level;
+    int line;
+    std::string_view file;
+    std::string_view scope;
+    std::string_view function;
+    std::string message;
+    TX::time::Time time;
+  };
+
+  class Reporter {
+   public:
+    virtual void report(const Log &);
+  };
+
+  class Formatter {
+   public:
+    virtual std::string format(const Log &);
+  };
+
+  static Logger::Level level_;
+  static Logger::Reporter *reporter_;
+  static Logger::Formatter *formatter_;
+  static void setLevel(Logger::Level level) { level_ = level; }
+  static void setReporter(Reporter *r) { reporter_ = r; }
+  static void setFormatter(Formatter *f) { formatter_ = f; }
+  static bool shouldLog(Logger::Level level) { return level >= level_; }
+};
+}  // namespace TX::logging

Source/TX/time/Clock.h
@@ -1,0 +1,66 @@
+#pragma once
+#include <cstdint>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <ctime>
+#endif
+
+namespace TX::time {
+class Clock {
+public:
+  enum struct Id {
+    Real = 0,
+    Monotonic = 6,
+  };
+  struct TimePoint {
+    int64_t sec;
+    int64_t nsec;
+  };
+  static Clock::TimePoint Now(Clock::Id id) {
+    Clock::TimePoint tp{};
+#ifdef _WIN32
+    switch (id) {
+    case Clock::Id::Real:
+      FILETIME ft;
+      GetSystemTimePreciseAsFileTime(&ft);
+      // 将 FileTime 转换为 100 纳秒单位
+      ULARGE_INTEGER uli; //
+      uli.LowPart = ft.dwLowDateTime;
+      uli.HighPart = ft.dwHighDateTime;
+      // 转换为 Unix 时间戳（从 1601-01-01 调整为 1970-01-01）
+      uli.QuadPart -= 116444736000000000ULL;
+      // 转换为秒和纳秒
+      tp.sec = uli.QuadPart / 10'000'000;
+      tp.nsec = (uli.QuadPart % 10'000'000) * 100;
+      break;
+    case Clock::Id::Monotonic:
+      static LARGE_INTEGER freq;
+      if (!inited) {
+        QueryPerformanceFrequency(&freq);
+        inited = true;
+      }
+      LARGE_INTEGER count;
+      QueryPerformanceCounter(&count);
+      tp.sec = count.QuadPart / freq.QuadPart;
+      tp.nsec = (count.QuadPart % freq.QuadPart) * 1000000000LL / freq.QuadPart;
+      break;
+    }
+#else
+    struct timespec ts {};
+    clock_gettime(static_cast<clockid_t>(id), &ts);
+    tp.sec = ts.tv_sec;
+    tp.nsec = ts.tv_nsec;
+#endif
+    return tp;
+  };
+
+  static Clock::TimePoint Real() { return Now(Clock::Id::Real); }
+  static Clock::TimePoint Monotonic() { return Now(Clock::Id::Monotonic); }
+
+private:
+  static bool inited;
+};
+
+inline bool Clock::inited = false;
+} // namespace TX::time

Source/TX/time/Duration.h
@@ -1,0 +1,67 @@
+#pragma once
+#include "TX/time/Clock.h"
+
+namespace TX::time {
+class Duration final {
+public:
+  explicit Duration(int64_t n) : inner(n) {}
+  static Duration Second(int n) { return Duration(n * 1'000'000'000); }
+  static Duration Minute(int n) { return Duration(n * 1'000'000'000 * 60); }
+  static Duration Hour(int n) { return Duration(n * 1'000'000'000 * 60 * 60); }
+
+  [[nodiscard]] int64_t NanoSeconds() const { return inner; }
+  [[nodiscard]] int64_t MicroSeconds() const { return inner / 1'000; }
+  [[nodiscard]] int64_t MilliSeconds() const { return inner / 1'000'000; }
+
+  [[nodiscard]] double Seconds() const {
+    int64_t sec = inner / 1'000'000'000;
+    int64_t nsec = inner % 1'000'000'000;
+    return static_cast<double>(sec) + static_cast<double>(nsec) / 1e9;
+  }
+  [[nodiscard]] double Minutes() const {
+    int64_t min = inner / (60 * 1'000'000'000LL);
+    int64_t nsec = inner % (60 * 1'000'000'000LL);
+    return static_cast<double>(min) + static_cast<double>(nsec) / (60 * 1e9);
+  }
+  [[nodiscard]] double Hours() const {
+    int64_t hour = inner / (60 * 60 * 1'000'000'000LL);
+    int64_t nsec = inner % (60 * 60 * 1'000'000'000LL);
+    return static_cast<double>(hour) +
+           static_cast<double>(nsec) / (60 * 60 * 1e9);
+  }
+
+  [[nodiscard]] Duration Truncate(Duration m) const {
+    return Duration(m.inner <= 0 ? inner : (inner - inner % m.inner));
+  }
+
+  Duration operator+(Duration &other) const {
+    return Duration(inner + other.inner);
+  }
+  Duration operator-(Duration &other) const {
+    return Duration(inner - other.inner);
+  }
+  Duration operator*(Duration &other) const {
+    return Duration(inner * other.inner);
+  }
+  Duration operator/(Duration &other) const {
+    return Duration(inner / other.inner);
+  }
+  Duration operator%(Duration &other) const {
+    return Duration(inner % other.inner);
+  }
+
+  [[nodiscard]] Clock::TimePoint TimePoint() const {
+    Clock::TimePoint tp{};
+    int64_t sec = inner / 1'000'000'000LL;
+    int64_t nsec = inner % 1'000'000'000LL;
+    tp.sec = sec;
+    tp.nsec = nsec;
+    return tp;
+  }
+
+private:
+  friend class Time;
+  int64_t inner;
+};
+
+}; // namespace TX::time

Source/TX/time/Time.h
@@ -1,0 +1,66 @@
+#pragma once
+#include "TX/time/Clock.h"
+#include "TX/time/Duration.h"
+
+namespace TX::time {
+constexpr uint64_t NANO_MASK = (1 << 30) - 1;
+
+class Time final {
+ public:
+  explicit Time() {
+    auto real_tp = Clock::Real();
+    auto mono_tp = Clock::Monotonic();
+    real_ = (real_tp.sec << 31) | (real_tp.nsec);
+    mono_ = mono_tp.sec * 1'000'000'000 + mono_tp.nsec;
+  }
+
+  [[nodiscard]] uint64_t Unix() const { return sec(); }
+  [[nodiscard]] uint64_t UnixMilli() const {
+    return sec() * 1'000 + nsec() / 1'000'000;
+  }
+  [[nodiscard]] uint64_t UnixMicro() const {
+    return sec() / 1'000'000 + nsec() / 1'000;
+  }
+  [[nodiscard]] uint64_t UnixNano() const {
+    return sec() * 1'000'000'000 + nsec();
+  }
+
+  Time operator+(Duration d) {
+    int64_t d_sec = d.inner / 1'000'000'000;
+    int64_t d_nsec = d.inner % 1'000'000'000;
+    uint64_t t_sec = sec();
+    uint64_t t_nsec = nsec();
+    real_ = ((t_sec + d_sec) << 31) | (t_nsec + d_nsec);
+    mono_ += d.inner;
+    return *this;
+  }
+  Time operator-(Duration d) {
+    int64_t d_sec = d.inner / 1'000'000'000;
+    int64_t d_nsec = d.inner % 1'000'000'000;
+    int64_t t_sec = sec();
+    int64_t t_nsec = nsec();
+    real_ = ((t_sec - d_sec) << 31) | (t_nsec - d_nsec);
+    mono_ -= d.inner;
+    return *this;
+  }
+  Duration operator-(Time other) {
+    return Duration((sec() - other.sec()) * 1'000'000'000 +
+                    (nsec() + other.nsec()));
+  }
+
+ private:
+  [[nodiscard]] int64_t sec() const {
+    return static_cast<int64_t>(real_ >> 31);
+  }
+  [[nodiscard]] int32_t nsec() const {
+    return static_cast<int32_t>(real_ & NANO_MASK);
+  }
+
+ private:
+  uint64_t real_;
+  uint64_t mono_;
+};
+
+inline Duration since(Time t) { return Time() - t; }
+inline Duration until(Time t) { return t - Time(); }
+}  // namespace TX::time